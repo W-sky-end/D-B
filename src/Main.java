769c147e20b8799f87fdf697661891331feeb9af
Vadim <aonyko@gmail.com>
2026-01-15 15:07:36 +0200

    add List

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,46 @@
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
-            System.out.println("Enter age for first and second persons:");
+            int count = 1;
+            System.out.println("Enter age for persons!");
+            List<User> users = new ArrayList<>();
 
-            System.out.print("Enter age for 1st person: ");
+            System.out.print("Enter age for " + count++ + " person: ");
             User vadim = new User("Vadim", sc.nextInt());
 
-            System.out.print("Enter age for 2nd person: ");
+            System.out.print("Enter age for " + count++ + " person: ");
             User mia = new User("Mia", sc.nextInt());
 
-            System.out.println(vadim + "\n" + mia);
+            System.out.println("Enter age for " + count++ + " person: ");
+            User mia2 = new User("Mia", sc.nextInt());
+            users.add(vadim);
+            users.add(mia);
+            users.add(mia2);
+            users.add(vadim);
+            System.out.println("Users : " + users);
+
+            Set<User> uniqueUsers = new HashSet<>(users);
+            System.out.println("Unique users : " + uniqueUsers);
+
+            Map<String,User> userMap = new HashMap<>();
+            userMap.put("Vadim",vadim);
+            userMap.put("Mia",mia);
+            userMap.put("Mia2",mia2);
+            userMap.put("Vadim2",vadim);
+
+            System.out.println("Users : " + userMap);
+
+
+
+            System.out.println(vadim + "\n" + mia + "\n" + mia2);
+            System.out.println("Users create : " + User.getUserCounter());
+            System.out.println("Set size: " + users.size());
+            System.out.println(users);
+
+
+
 
         } catch (InputMismatchException e) {
             System.out.println("Age must be a number.");
--- src/User.java
+++ src/User.java
@@ -1,12 +1,17 @@
+import java.util.Objects;
+
 public class User {
     private final String name;
     private int age;
+    private static int userCounter = 0;
+    public static final int MAX_AGE = 50;
 
 
     public User(String name, int age) {
         validate(name, age);
         this.name = name;
         this.age = age;
+        userCounter ++;
     }
 
     public String getName() {
@@ -18,21 +23,36 @@ public class User {
     }
 
     public void setAge(int age) {
-        if (age < 0)
-            throw new IllegalArgumentException("Age cannot be negative");
+        if (age < 0 || age >= MAX_AGE)
+            throw new IllegalArgumentException("Age must be between 0 and " + (MAX_AGE - 1));
         this.age = age;
+    }
+
+    public static int getUserCounter() {
+        return userCounter;
     }
 
     @Override
     public String toString() {
-        return "User :" +
+        return "\nUser :" +
                 "\nName --- " + name +
                 "\nAge --- " + age +
                 "\n---------------";
     }
     private void validate (String name,int age){
         if (name == null || name.isBlank()) throw new IllegalArgumentException("Name cannot be blank");
-        if (age < 0) throw new IllegalArgumentException("Age cannot be negative");
+        if (age < 0 || age >= MAX_AGE) throw new IllegalArgumentException("Age must be between 0 and " + (MAX_AGE - 1));
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
+    }
 }
